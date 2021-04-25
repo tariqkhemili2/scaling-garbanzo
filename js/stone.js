f94712e9170c264d6cb4b2d5ef9222e7736482ee
tariqkhemili2 <74871284+tariqkhemili2@users.noreply.github.com>
2021-04-25 16:10:14 +0100

    Add files via upload

---
+++ js/stone.js
@@ -0,0 +1,14 @@
+class Stone {
+    constructor(x,y){
+      super(x,y,50,50);
+      this.image = loadImage("images/stone.png");
+    }
+  
+    display() {
+      //this.body.position.x = mouseX;
+      //this.body.position.y = mouseY;
+  
+      super.display();
+    }
+  }
+  
